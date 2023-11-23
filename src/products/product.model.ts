3f85c086c00a2abfbf7e2e267b0dbc7e172429f3
Tommy Bautista <tommybautista320@gmail.com>
2023-11-23 08:52:17 -0500

    modified

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -4,7 +4,5 @@ export class Product {
         public title: string,
         public description: string,
         public price: number
-    ) {
-
-    }
+    ) {}
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { Product } from "./product.model";
 
 @Injectable()
 export class ProductsService {
-    
+
+    products: Product[] = [];
+
+    constructor() {}
+
+    insertProduct(
+        title: string,
+        desc: string,
+        price: number
+    ) {
+        const newProduct = new Product(new Date().toString(),title, desc, price);
+        this.products.push(newProduct);
+    }
 
 }
